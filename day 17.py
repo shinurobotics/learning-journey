1636bb11e0c8f5ac7c96c7fe4aebc85a84ebd261
shinurobotics <shinu0848@gmail.com>
2026-06-09 17:44:42 +0800

    Create day 17.py

---
+++ day 17.py
@@ -0,0 +1,23 @@
+ParticipantNumber = 2
+ParticipantData = []
+registeredParticipants = 0
+OutputFile = open("ParticipantData.txt","w")
+
+while (registeredParticipants < ParticipantNumber):
+    tempPartData = [] #name,age,origin
+    name = input("Please type your name:\n")
+    tempPartData.append(name)
+    age = int(input("Please type your age:\n"))
+    tempPartData.append(age)
+    country = input("Please type your origin country:\n")
+    tempPartData.append(country)
+    ParticipantData.append(tempPartData)
+    registeredParticipants += 1
+print(ParticipantData)
+
+for participants in ParticipantData:
+    for data in participants:
+        OutputFile.write(str(data))
+        OutputFile.write(" ")
+    OutputFile.write("\n")
+OutputFile.close()
