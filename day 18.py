9974b5fa3a86194a389cb1db6512942b1baa13b8
shinurobotics <shinu0848@gmail.com>
2026-06-10 17:46:32 +0800

    Create day 18.py

---
+++ day 18.py
@@ -0,0 +1,34 @@
+ParticipantNumber = 2
+ParticipantData = []
+registeredParticipants = 0
+OutputFile = open("ParticipantData.txt","w")
+
+while (registeredParticipants < ParticipantNumber):
+    tempPartData = [] #name,age,origin
+    name = input("Please type your name:\n")
+    tempPartData.append(name)
+    age = int(input("Please type your age:\n"))
+    tempPartData.append(age)
+    country = input("Please type your origin country:\n")
+    tempPartData.append(country)
+    ParticipantData.append(tempPartData)
+    registeredParticipants += 1
+print(ParticipantData)
+
+for participants in ParticipantData:
+    for data in participants:
+        OutputFile.write(str(data))
+        OutputFile.write(" ")
+    OutputFile.write("\n")
+OutputFile.close()
+
+InputFile = open("ParticipantData.txt","r")
+InputList = []
+for line in InputFile:
+    tempParticipant = line.strip("\n").split()
+    print(tempParticipant)
+    InputList.append(tempParticipant)
+    print(InputList)
+     #"Ronaldo 41 Portugal \n".strip() -> "Ronaldo 41 Portugal"
+     #"Ronaldo 41 Portugal".split() -> ["Ronaldo","41","Portugal"]
+InputFile.close()
