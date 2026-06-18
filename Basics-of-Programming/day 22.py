fe4fe459d007dff0ea718584634d227a14ae9ba1
shinurobotics <shinu0848@gmail.com>
2026-06-18 17:50:19 +0800

    Create day 22.py

---
+++ Basics-of-Programming/day 22.py
@@ -0,0 +1,25 @@
+import math
+
+Val = math.pi
+sqrtVal = math.sqrt(Val)
+print(sqrtVal)
+
+# var*int -> multiplication
+# var**int -> squaring
+
+expVal = math.exp(Val)
+print(expVal)
+
+#factVal = math.factorial(Val)
+#print(factVal)
+
+sinVal = math.sin(Val)
+print(sinVal)
+
+floorVal = math.floor(Val)
+print(floorVal)
+
+ceilVal = math.ceil(Val)
+print(ceilVal)
+
+https://docs.python.org/3/library/math.html math library reference
