996727cbdc5162761836fb29f5978dd0dd03713d
shinurobotics <shinu0848@gmail.com>
2026-06-19 18:07:40 +0800

    Create day 26.py

---
+++ Basics-of-Programming/day 26.py
@@ -0,0 +1,43 @@
+#float("123.4") -> 123.4
+#float("N/A") -> error
+
+#General form
+#try:
+
+#except:
+
+#Form 1
+try:
+    print(int("Hello")) 
+except:
+    print("Entered exception")
+print("Past exception")
+
+#Form 2
+keyword = "123"
+try:
+    print(int(keyword))
+except:
+    print("Entered exception")
+print("Past exception")
+
+#Form 3
+try:
+    print(int("Visnu"))
+except:
+    pass
+print("Past exception")
+
+#Form 4
+key = "Hello"
+try:
+    print(int(key))
+except ValueError: # or except Exception as e:
+    print("got a ValueError")
+except:
+    print("Other type of exception")
+finally:
+    print("Finally")
+print("Past exception")
+
+Stops at 14:40
