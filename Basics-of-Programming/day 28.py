878cf67659128dc4c2d691a2d75ac5089ea17e52
shinurobotics <shinu0848@gmail.com>
2026-06-25 17:43:07 +0800

    Update day 28.py

--- Basics-of-Programming/day 28.py
+++ Basics-of-Programming/day 28.py
@@ -15,12 +15,15 @@ cardDeck = createDeck()
 print(cardDeck)
 
 class Player:
-    def _init_(self,hand = [],money = 100):
+    def __init__(self,hand = [],money = 100):
         self.hand = hand
-        self.score = 0
+        self.score = 0 #"3 4" -> " 3 4 10"
+                       # 7 -> 7 + 10
+        self.score = self.setScore()
+        print(self.score)
         self.money = money
     
-    def _str_(self):       #print(Player)
+    def __str__(self):       #print(Player)
         currentHand = " "  #self.hand = ["A","10"]
                            #"A 10"
         for card in self.hand:
@@ -29,12 +32,20 @@ class Player:
         #"A 10 2 4 score: 17"
         return finalstatus
     
-    def setScore():
-        faceCardsDict = {"A":11,"J":10,"K":10}
+    def setScore(self):
+        faceCardsDict = {"A":11,"J":10,"K":10,"Q":10,"2":2,"3":3,"4":4,
+        "5":5,"6":6,"7":7,"8":8,"9":9,"10":10}
+        print(self.score)
+        acecounter = 0
         for card in self.hand: #"10"
-        if int(card) in range(2,11):
-            self.score += int(card)
-        elif card in faceCardsDict: #"A"
-            self.score += faceCardsDict[]
-
-stops at 19:15
+            self.score += faceCardsDict[card]
+            if card == "A":
+                acecounter += 1
+            if self.score > 21 and acecounter != 0:
+                self.score -= 10
+                acecounter -= 1
+        return self.score
+            
+Player1 = Player(["3","7","5"])               
+print(Player1)
+                
