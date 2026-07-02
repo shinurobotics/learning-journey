fa381a91f8ccfeda31c9f3f012ae9d78d573d91c
shinurobotics <shinu0848@gmail.com>
2026-07-02 18:22:15 +0800

    Create day 4.py

---
+++ Python-Basics-from-Brilliance-in-HD/day 4.py
@@ -0,0 +1,27 @@
+name = input("Please type your name: ")
+Screen = "Welcome to Python"
+print("Welcome "+ name + "\n" + Screen + "\nHave a great day!\n" )
+print("Event info")
+print("1.Start the game \n" + "2.Win the level\n" + "3.Be the Champion")
+
+Favteam = input("Please type the country you support in WC 2026: ")
+print("Team supporting:\t" + Favteam)
+
+Web = "www.google.com"
+print("https:\\\\" + Web)
+
+File = "Python"
+InputFile = "Basics of Programming"
+print(File + "\\" + InputFile + "\\" + "day 1 progress")
+
+Talk = input("Say something: ")
+print(name + " said that " + "\"" + Talk + "\"")
+
+Programming = "Python"
+print("I learn " + "\'" + Programming + "\'")
+
+#\n -> add new line
+#\t -> add tab space
+#\\ -> add \
+#\" -> add "
+#\' -> add '
