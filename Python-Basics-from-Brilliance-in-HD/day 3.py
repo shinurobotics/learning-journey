35d5d8538394bdc9d35f27b51cada0451e16eef3
shinurobotics <shinu0848@gmail.com>
2026-07-02 17:46:30 +0800

    Create day 3.py

---
+++ Python-Basics-from-Brilliance-in-HD/day 3.py
@@ -0,0 +1,23 @@
+# 1 as True Value
+# 0 as False Value
+# Alternative True -> not False
+# Alternative False -> not True
+
+a,b = not False,not True
+print(a)
+print(b)
+
+correctPassword = "Python123"
+Password = input("Please type the password: ")
+isCorrectPassword = Password == correctPassword
+print(isCorrectPassword)
+
+age = int(input("What is your age?"))
+canVote = age >=18
+if canVote:
+    print(canVote)
+    print("You are to eligible to vote")
+else:
+    print(canVote)
+    print("You are not eligible to vote")
+  
