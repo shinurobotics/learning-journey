812fccda8293587f8a4f9a3184020d38d41955e0
shinurobotics <shinu0848@gmail.com>
2026-07-03 17:44:20 +0800

    Create day 5.py

---
+++ Python-Basics-from-Brilliance-in-HD/day 5.py
@@ -0,0 +1,12 @@
+#title.() -> capitalizing first letter
+
+city = "klang"
+print(city.title())
+
+Firstname = "visnu"
+Lastname = "kumar"
+print(Firstname.title() + " " + Lastname.title())
+
+students = ["rahul", "Riyyan", "dhars"]
+for names in students:
+    print(names.title())
