e5c92af9d1bd1cb795203f970839100ec024dd74
shinurobotics <shinu0848@gmail.com>
2026-07-04 17:53:37 +0800

    Create day 07.py

---
+++ Python-Basics-from-Brilliance-in-HD/day 07.py
@@ -0,0 +1,24 @@
+VacPlaces = ["Thailand","Malaysia","Turkey","Singapore"]
+VacPlaces.sort()
+print(VacPlaces)
+
+Element = ["Oxygen","Nitrogen","Carbon","Sodium"]
+Element.reverse()
+print(Element)
+
+Jobs = ["Doctor","Lawyer", "Police","Teacher"]
+Jobs.sort(reverse = True)
+# reverse = True then a to z, reverse = False then z to a
+print(Jobs)
+
+Marks = [45,98,87,53,76]
+Marks.sort()
+print("Ascending:",Marks)
+Marks.reverse()
+print("Descending:",Marks)
+
+age = [11,31,52,98,46]
+print(min(age))
+print(max(age))
+Avgage = sum(age)/len(age)
+print(Avgage)
